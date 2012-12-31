8abd488aa215e7c20aec35515a8aa1db62d6aa1f
Lukas Ruebbelke <simpul@gmail.com>
2012-12-30 18:16:31 -0700

    Initial code commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+	app = express();
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
+});
+
+io.sockets.on('connection', function(socket) {
+	socket.on('createNote', function(data) {
+		socket.broadcast.emit('onNoteCreated', data);
+	});
+
+	socket.on('updateNote', function(data) {
+		socket.broadcast.emit('onNoteUpdated', data);
+	});
+
+	socket.on('moveNote', function(data){
+		socket.broadcast.emit('onNoteMoved', data);
+	});
+
+	socket.on('deleteNote', function(data){
+		socket.broadcast.emit('onNoteDeleted', data);
+	});
+});
+
+server.listen(1337);
